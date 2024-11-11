d9fcf507911c3acc43b5d45f74367a6514c9a2c5
thatguy988 <kirbyj3684@tamu.edu>
2024-11-11 20:55:02 +0000

    refactored and changed train and main

--- RL_Project/main.py
+++ RL_Project/main.py
@@ -1,9 +1,12 @@
-from train import train_baseline_dqn, train_categorical_dqn, train_noisy_dqn, train_noisy_categorical_dqn
+from train import train_dqn
 from evaluate import evaluate_dqn
 from stable_baselines3 import DQN
 from algorithms.categorical_dqn import CategoricalDQN
 from algorithms.noisy_dqn import NoisyDQN
 from algorithms.noisy_categorical_dqn import NoisyCategoricalDQN
+from algorithms.categorical_dqn import create_categorical_dqn
+from algorithms.noisy_dqn import create_noisy_dqn
+from algorithms.noisy_categorical_dqn import create_noisy_categorical_dqn
 import gymnasium as gym
 from stable_baselines3.common.vec_env import DummyVecEnv
 
@@ -13,29 +16,34 @@ def main():
     env = gym.make("ALE/Seaquest-v5")
     env = DummyVecEnv([lambda: env])
     #train baseline dqn
-    # dqn_model_path = train_baseline_dqn(env)
-    # dqn_model = DQN.load(dqn_model_path)
-    # mean_reward, std_reward, max_reward = evaluate_dqn(dqn_model, env)
+    # Create the DQN model
+    baseline_model = DQN("CnnPolicy", env, buffer_size=30000, learning_rate= 1e-4, exploration_fraction = 0.3, exploration_final_eps=0.05, verbose=1)
+    dqn_model_path = train_dqn(baseline_model, env, "baseline_dqn_model")
+    dqn_model = DQN.load(dqn_model_path)
+    mean_reward, std_reward, max_reward = evaluate_dqn(dqn_model, env)
+    print(f"Evaluation results for baseline: Mean reward: {mean_reward:.2f}, Std reward: {std_reward:.2f}, Max reward: {max_reward:.2f}")
 
     #train categorical dqn
-    # c51_model_path = train_categorical_dqn(env)
-    # c51_model = CategoricalDQN.load(c51_model_path)
-    # mean_reward, std_reward, max_reward = evaluate_dqn(c51_model, env)
+    c51_model = create_categorical_dqn(env, n_atoms=51, v_min=-10, v_max=10, verbose=1)
+    c51_model_path = train_dqn(c51_model, env, "c51_dqn_model")
+    c51_model = CategoricalDQN.load(c51_model_path)
+    mean_reward, std_reward, max_reward = evaluate_dqn(c51_model, env)
+    print(f"Evaluation results for c51: Mean reward: {mean_reward:.2f}, Std reward: {std_reward:.2f}, Max reward: {max_reward:.2f}")
 
     #train noisy dqn
-    # noisy_model_path = train_noisy_dqn(env)
-    # noisy_model = NoisyDQN.load(noisy_model_path)
-    # mean_reward, std_reward, max_reward = evaluate_dqn(noisy_model, env)
+    noisy_model = create_noisy_dqn(env)
+    noisy_model_path = train_dqn(noisy_model, env, "noisy_dqn_model")
+    noisy_model = NoisyDQN.load(noisy_model_path)
+    mean_reward, std_reward, max_reward = evaluate_dqn(noisy_model, env)
+    print(f"Evaluation results for noisy: Mean reward: {mean_reward:.2f}, Std reward: {std_reward:.2f}, Max reward: {max_reward:.2f}")
 
     # train noisy/categorical dqn variant
-    noisy_categorical_model_path = train_noisy_categorical_dqn(env)
+    noisy_categorical_model = create_noisy_categorical_dqn(env, n_atoms=51, v_min=-10, v_max=10, verbose=1)
+    noisy_categorical_model_path = train_dqn(noisy_categorical_model, env, "noisy_categorical_variant_model")
     noisy_categorical_model = NoisyCategoricalDQN.load(noisy_categorical_model_path)
     mean_reward, std_reward, max_reward = evaluate_dqn(noisy_categorical_model, env)
+    print(f"Evaluation results for noisy/c51 variant: Mean reward: {mean_reward:.2f}, Std reward: {std_reward:.2f}, Max reward: {max_reward:.2f}")
 
-
-    print(f"Evaluation results: Mean reward: {mean_reward:.2f}, Std reward: {std_reward:.2f}, Max reward: {max_reward:.2f}")
-
-    
 
 if __name__ == "__main__":
     main()
--- RL_Project/train.py
+++ RL_Project/train.py
@@ -1,9 +1,5 @@
 
 import gymnasium as gym
-from stable_baselines3 import DQN
-from algorithms.categorical_dqn import create_categorical_dqn
-from algorithms.noisy_dqn import create_noisy_dqn
-from algorithms.noisy_categorical_dqn import create_noisy_categorical_dqn
 import os
 import torch
 import torch.optim as optim
@@ -11,19 +7,10 @@ import torch.nn.functional as F
 from stable_baselines3.common.buffers import ReplayBuffer
 import numpy as np
 
-def train_baseline_dqn(env):
-
-    # Create the DQN model
-    model = DQN("CnnPolicy", 
-    env, 
-    buffer_size=10000, 
-    learning_rate= 1e-4, 
-    exploration_fraction = 0.3, 
-    exploration_final_eps=0.05, 
-    verbose=1)
+def train_dqn(model, env, total_timesteps=200000, model_name="model"):
 
     # Train the model
-    model.learn(total_timesteps=200000) 
+    model.learn(total_timesteps=total_timesteps) 
 
     # Define the path to the models directory
     models_dir = "/content/drive/MyDrive/models"
@@ -32,79 +19,10 @@ def train_baseline_dqn(env):
     os.makedirs(models_dir, exist_ok=True)
 
     # Save model
-    model_path = os.path.join(models_dir, "baseline_dqn_seaquest")
+    model_path = os.path.join(models_dir, f"{model_name}")
     model.save(model_path)
 
     print(f"Model saved to {model_path}")
 
     return model_path 
-
-def train_categorical_dqn(env):
-  # the number of atoms and value range v_min to v_max is crucial
-  # paper recommendations for atom to range ratios 
-  # aim for ~0.4 to 0.5 atoms per unit of value range
-  # range (-10 to 10) 51 atoms
-  # range (-50 to 50) 51 atoms
-  # range (-100 to 100) 81 atoms 
-  model = create_categorical_dqn(env, n_atoms=51, v_min=-10, v_max=10, verbose=1)
-
-
-  # Train the model
-  model.learn(total_timesteps=200000) 
-
-  # Define the path to the models directory
-  models_dir = "/content/drive/MyDrive/models"
-
-  # Ensure the models directory exists
-  os.makedirs(models_dir, exist_ok=True)
-
-  # Save the model
-  model_path = os.path.join(models_dir, "c51_dqn_seaquest")
-  model.save(model_path)
-
-  print(f"Model saved to {model_path}")
-
-  return model_path  
-
-def train_noisy_dqn(env):
-
-  model = create_noisy_dqn(env)
-
-  # Train the model
-  model.learn(total_timesteps=200000) 
-
-  # Define the path to the models directory
-  models_dir = "/content/drive/MyDrive/models"
-
-  # Ensure the models directory exists
-  os.makedirs(models_dir, exist_ok=True)
-
-  # Save model
-  model_path = os.path.join(models_dir, "noisy_dqn_seaquest")
-  model.save(model_path)
-
-  print(f"Model saved to {model_path}")
-
-  return model_path 
-
-def train_noisy_categorical_dqn(env):
-
-  model = create_noisy_categorical_dqn(env, n_atoms=51, v_min=-10, v_max=10, verbose=1)
-
-  # Train the model
-  model.learn(total_timesteps=60000) 
-
-  # Define the path to the models directory
-  models_dir = "/content/drive/MyDrive/models"
-
-  # Ensure the models directory exists
-  os.makedirs(models_dir, exist_ok=True)
-
-  # Save model
-  model_path = os.path.join(models_dir, "noisy_categorical_dqn_seaquest")
-  model.save(model_path)
-
-  print(f"Model saved to {model_path}")
-
-  return model_path
  
